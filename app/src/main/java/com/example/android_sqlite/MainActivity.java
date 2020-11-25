1dcb12baebfb1907f89d6d55e18079eee6e47527
Raul Paulino <20461484+rfire12@users.noreply.github.com>
2020-11-25 00:08:38 -0400

    list

--- app/src/main/java/com/example/android_sqlite/MainActivity.java
+++ app/src/main/java/com/example/android_sqlite/MainActivity.java
@@ -6,9 +6,17 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ProductsListFragment productsListFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        productsListFragment = new ProductsListFragment();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.container, productsListFragment)
+                .commit();
     }
 }
---
+++ app/src/main/java/com/example/android_sqlite/ProductsListFragment.java
@@ -0,0 +1,83 @@
+package com.example.android_sqlite;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ProductsListFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ProductsListFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private ListView productsListView;
+
+    public ProductsListFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ProductsListFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ProductsListFragment newInstance(String param1, String param2) {
+        ProductsListFragment fragment = new ProductsListFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_products_list, container, false);
+
+        String[] items = {
+                "PC",
+                "Mouse"
+        };
+        productsListView = (ListView) view.findViewById(R.id.productsListView);
+
+        ArrayAdapter<String> productsListAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                items
+        );
+
+        productsListView.setAdapter(productsListAdapter);
+
+        return inflater.inflate(R.layout.fragment_products_list, container, false);
+    }
+}
