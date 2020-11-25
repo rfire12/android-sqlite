050d7e5ddb9523909e4d37896309bd752c42a5c0
Raul Paulino <20461484+rfire12@users.noreply.github.com>
2020-11-25 11:17:37 -0400

    add menu

--- app/src/main/java/com/example/android_sqlite/MainActivity.java
+++ app/src/main/java/com/example/android_sqlite/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.android_sqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +18,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        productsListFragment = new ProductsListFragment();
-        getSupportFragmentManager()
-                .beginTransaction()
-                .add(R.id.container, productsListFragment)
-                .commit();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if( id == R.id.addButton ) {
+            Toast.makeText(getApplicationContext(), "clicked", Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 }
